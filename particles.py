5e62b45401758b2dc6df85956cd6a3ed1348acf9
Joseph Langford <Joseph@eutune.com>
2017-01-02 10:44:08 -0500

    Added particles.py
    Going to use this to learn particle systems

---
+++ particles.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init()
+# Formulas To Remember
+# speed = (change in position / change in time)
+# acceleration = (change in speed / change in time)
+
+class Particle():
+    def __init___(self):
+        self.x
