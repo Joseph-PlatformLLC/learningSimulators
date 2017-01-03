1c7e65a5413c10726099f2958b6c5274ff19f22c
Joseph Langford <Joseph@eutune.com>
2017-01-03 12:33:56 -0500

    Second_Commit

--- particles.py
+++ particles.py
@@ -1,9 +1,32 @@
 import pygame
 pygame.init()
+sDisplay = pygame.display.set_mode(( 1000 , 800 ))
+
 # Formulas To Remember
+# force = mass * acceleration
+# displacement = Velocity * time
 # speed = (change in position / change in time)
 # acceleration = (change in speed / change in time)
+# v_after = -(Elasticity) * Velocity
+
+
+#Vectors: magnitude and direction -> displacement
+#Scalars: just magnitude          -> velocity (no direction)
+
+
+RED = (255,0,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREY = (128, 131, 137)
+
+# gravity
+# wind
+# collision
+# alive
+# onScreen
+# heat
+# air temperature
+
 
 class Particle():
     def __init___(self):
-        self.x
